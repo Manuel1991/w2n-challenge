ee907f13a020f0c128c57d1fd156d948468a04fa
ManuelCode1991 <fochoa@folcode.com>
2023-04-13 16:32:35 -0300

    caching

--- src/main/java/com/w2n/challenge/controllers/HeroController.java
+++ src/main/java/com/w2n/challenge/controllers/HeroController.java
@@ -4,6 +4,11 @@ import com.w2n.challenge.Utils.PageableUtils;
 import com.w2n.challenge.domain.dtos.HeroResponseDTO;
 import com.w2n.challenge.domain.dtos.NewHeroDTO;
 import com.w2n.challenge.services.HeroService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -25,7 +30,9 @@ public class HeroController {
     }
 
     @GetMapping
+    @Cacheable(cacheNames = "heroes", key = "#request.getRequestURI() + '?' + #request.getQueryString()")
     public ResponseEntity<Page<HeroResponseDTO>> getAllHeroes(
+            HttpServletRequest request,
             @RequestParam(defaultValue = "0") int pageNumber,
             @RequestParam(defaultValue = "10") int pageSize,
             @RequestParam(defaultValue = "ASC") String orderCriteria,
@@ -43,11 +50,13 @@ public class HeroController {
     }
 
     @GetMapping("/{id}")
+    @Cacheable(value = "hero", key = "#id")
     public ResponseEntity<HeroResponseDTO> getHeroById(@PathVariable UUID id) {
         return ResponseEntity.ok(heroService.getHeroById(id));
     }
 
     @PostMapping
+    @CacheEvict(value = "heroes", allEntries = true)
     public ResponseEntity<HeroResponseDTO> createHero(
             @RequestBody NewHeroDTO newHeroDTO
     ) {
@@ -57,6 +66,11 @@ public class HeroController {
     }
 
     @PutMapping("/{id}")
+    @Caching(
+            evict = {@CacheEvict(value = "heroes", allEntries = true)},
+            put = {@CachePut(value = "hero", key = "#id")}
+    )
+    @CachePut(value = "hero", key = "#id")
     public ResponseEntity<HeroResponseDTO> updateHero(
             @PathVariable UUID id,
             @RequestBody NewHeroDTO heroDTO
@@ -67,6 +81,10 @@ public class HeroController {
     }
 
     @DeleteMapping("/{id}")
+    @Caching(evict = {
+            @CacheEvict(value = "heroes", allEntries = true),
+            @CacheEvict(value = "hero", key = "#id")
+    })
     public ResponseEntity<HeroResponseDTO> deleteHero(@PathVariable UUID id) {
         heroService.deleteHero(id);
         return ResponseEntity.noContent().build();
